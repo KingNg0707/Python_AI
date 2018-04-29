13dfb4ec33f1138c090eaddaf314def52490c713
KingNg0707 <zhaojinwu126@gmail.com>
2018-04-29 23:10:14 +0900

    删除多余注释

--- arcs.py
+++ arcs.py
@@ -4,7 +4,6 @@ import random
 import threading
 
 
-# #画圆1
 class Arc(threading.Thread):
     _arc = None
     _canvas = None
@@ -22,9 +21,13 @@ class Arc(threading.Thread):
     def run(self):
         print(threading.current_thread())
         while True:
-            self.event.wait()
-            self.arc_rand_move()
-            self.arc_rand_rotate()
+            try:
+                self.event.wait()
+                self.arc_rand_move()
+                self.arc_rand_rotate()
+            except:
+                print(f"{self.name} thread end.")
+                break
 
     def arcs_init(self, name, x, y, sx, sy, start, extent, color, index):
         self.name = name
@@ -41,7 +44,6 @@ class Arc(threading.Thread):
     def get_name(self):
         return self.name
 
-    # ####移动一个圆
     def arc_move(self):
         # 旋转角度坐标移动的计算
         x = 1 * math.sin(math.radians(self.startC - 30 + 270))
@@ -70,109 +72,22 @@ class Arc(threading.Thread):
 
         if random.randint(0, 5) < 3:  # 前进
             self.arc_move()
-            # mycanvas.move(canvas_arctest,2,2)
-            # canvas_arctest.move(mycanvas)
-            time.sleep(0.01)
+
+            time.sleep(0.02)
 
     def arc_rand_rotate(self):
 
         if random.randint(0, 5) < 3:  # 转向
 
             self.arc_rotate_lr(random.randint(0, 2))
-            time.sleep(0.01)
-        # if who.index == 1:
-        #     global canvas_arc1
-        #     mycanvas.delete(canvas_arc1)
-        #     canvas_arc1 = mycanvas.create_arc(who.xy,
-        #                                       who.xy[0] + who.size[0],
-        #                                       who.xy[1] + who.size[1],
-        #                                       start=who.startC,
-        #                                       extent=who.extentC,
-        #                                       fill=who.color)
-        # elif who.index == 2:
-        #     global canvas_arc2
-        #     mycanvas.delete(canvas_arc2)
-        #     canvas_arc2 = mycanvas.create_arc(who.xy,
-        #                                       who.xy[0] + who.size[0],
-        #                                       who.xy[1] + who.size[1],
-        #                                       start=who.startC,
-        #                                       extent=who.extentC,
-        #                                       fill=who.color)
-        # elif who.index == 3:
-        #     global canvas_arc3
-        #     mycanvas.delete(canvas_arc3)
-        #     canvas_arc3 = mycanvas.create_arc(who.xy,
-        #                                       who.xy[0] + who.size[0],
-        #                                       who.xy[1] + who.size[1],
-        #                                       start=who.startC,
-        #                                       extent=who.extentC,
-        #                                       fill=who.color)
+            time.sleep(0.02)
 
-    #
-    # # ##随机移动 圆1
-    # def arc1_randmove():
-    #     arc_randmove(arc1, canvas_arc1)
-    #     # print(arc_check_place(arc1))
-    #     # print(arc_check_Rotate(arc1))
-    #
-    #     # arc_randRotate(arc1)
-    #
-    #
-    # # ##随机移动 圆2
-    # def arc2_randmove():
-    #     arc_randmove(arc2, canvas_arc2)
-    #     # arc_randRotate(arc2)
-    #
-    #
-    # # ##随机移动 圆3
-    # def arc3_randmove():
-    #     arc_randmove(arc3, canvas_arc3)
-    #     # arc_randRotate(arc3)
-    #
-    #
-    # def arc4_randmove():
-    #     arc_randmove(arc4, canvas_arc4)
-    #
-    # def arcs_rand_move(self, arcst, canvas_arcst, canvas):
-    #     self.arc_randmove(arcst[0], canvas_arcst[0], canvas)
-    #
-    # # ##随机旋转 圆1
-    # def arc1_rand_rotate(self, arc, canvas_arcst, canva):
-    #     self.arc_rand_rotate(arc, canvas_arcst, canva)
+    def arc_check_place(self):
+        return self.xy
 
-    # # ##随机旋转 圆2
-    # def arc2_rand_rotate(self):
-    #     self.arc_rand_rotate(self.arc2)
-    #
-    # # ##随机旋转 圆3
-    # def arc3_rand_rotate(self):
-    #     self.arc_rand_rotate(self.arc3)
-    #
-    # def arcs_rand_rotate(self):
-    #     self.arc_rand_rotate(self.arcst[0])
+    def arc_check_rotate(self):
+        return self.startC
 
-    # def arc_check_edge():
-    #    if
-    # def arc1_check_edge():
-    #    if
-    # def arc2_check_edge():
-    #    if
-    # def arc3_check_edge():
-    #    if
-
-#
-#
-# def arctest_creat(self, window):
-#     window.create_arc(self.xy, self.xy[0] + self.size[0], self.xy[1] + self.size[1], start=self.startC,
-#                       extent=self.extentC, fill=self.color)
-#
-#
-# def arc_move(xy):
-#     x = 1 * math.sin(math.radians(arc1_start - 30 + 270))
-#     y = 1 * math.cos(math.radians(arc1_start - 30 + 270))
-#
-#     canvas.move(arc1, x, y)
-#
-#     arc1_xy = [arc1_xy[0] + x, arc1_xy[1] + y, arc1_xy[0] + x + 20, arc1_xy[1] + y + 20]
-#
-#     return xy
+    def arc_check_edge(self):
+        if self.arc_check_place() >= 400:
+            pass
--- creatures.py
+++ creatures.py
@@ -6,45 +6,32 @@ class Creature():
     def __init__(self, mycanvas):
         self._mycanvas = mycanvas
         self._arc = Arc
-        self.start = False
         self.arcs_list = []
         self.canvas_arcst = []
 
         self.arc_list = ["arc1", 20, 20, 20, 20, 120, 300, "red", 1], \
                         ["arc2", 70, 70, 20, 20, 120, 300, "blue", 2], \
                         ["arc3", 120, 120, 20, 20, 120, 300, "green", 3], \
-                        ["arc4", 170, 170, 20, 20, 120, 300, "yellow", 4]
-        #
-        # self.arc1 = self._arcs.arcs_init("arc1", 10, 10, 20, 20, 120, 300, "black", 1)
-        # self.arc2 = self._arcs.arcs_init("arc2", 60, 60, 20, 20, 120, 300, "black", 2)
-        # self.arc3 = self._arcs.arcs_init("arc3", 110, 110, 20, 20, 120, 300, "black", 3)
-        # self.arc4 = self._arcs.arcs_init("arc4", 160, 160, 20, 20, 120, 300, "black", 4)
-        #
-        # self.canvas_arc1 = None
-        # self.canvas_arc2 = None
-        # self.canvas_arc3 = None
-        # self.canvas_arc4 = None
+                        ["arc4", 170, 170, 20, 20, 120, 300, "yellow", 4], \
+                        ["arc5", 220, 220, 20, 20, 120, 300, "grey", 5]
 
     def arcs_init(self):
+
         # 初始化圆的值
         for i in range(len(self.arc_list)):
             self.arcs_list.append(self._arc())
             self.arcs_list[i].event.clear()
             self.arcs_list[i].arcs_init(name=self.arc_list[i][0],
-                                        x=self.arc_list[i][1],
-                                        y=self.arc_list[i][2],
-                                        sx=self.arc_list[i][3],
-                                        sy=self.arc_list[i][4],
-                                        start=self.arc_list[i][5],
-                                        extent=self.arc_list[i][6],
+                                        x=self.arc_list[i][1], y=self.arc_list[i][2],
+                                        sx=self.arc_list[i][3], sy=self.arc_list[i][4],
+                                        start=self.arc_list[i][5], extent=self.arc_list[i][6],
                                         color=self.arc_list[i][7],
                                         index=self.arc_list[i][8])
 
             arc = self._mycanvas.create_arc(self.arcs_list[i].xy,
                                             self.arcs_list[i].xy[0] + self.arcs_list[i].size[0],
                                             self.arcs_list[i].xy[1] + self.arcs_list[i].size[1],
-                                            start=self.arcs_list[i].startC,
-                                            extent=self.arcs_list[i].extentC,
+                                            start=self.arcs_list[i].startC, extent=self.arcs_list[i].extentC,
                                             fill=self.arcs_list[i].color)
             self.arcs_list[i]._canvas = self._mycanvas
             self.arcs_list[i]._arc = arc
@@ -55,74 +42,11 @@ class Creature():
         for i in self.arcs_list:
             i.start()
 
-    #     self.arcs_creat(mycanvas)
-    #
-    # # 创建空间
-    # # [a*b for a, b in zip(arc1_xy,arc1_size)]
-    # def arcs_creat(self, mycanvas):
-    #     # 创建3个圆形
-    #     canvas_arc1 = mycanvas.create_arc(self.arc1.xy, self.arc1.xy[0] + self.arc1.size[0],
-    #                                       self.arc1.xy[1] + self.arc1.size[1],
-    #                                       start=self.arc1.startC,
-    #                                       extent=self.arc1.extentC, fill=self.arc1.color)
-    #     canvas_arc2 = mycanvas.create_arc(self.arc2.xy, self.arc2.xy[0] + self.arc2.size[0],
-    #                                       self.arc2.xy[1] + self.arc2.size[1],
-    #                                       start=self.arc2.startC,
-    #                                       extent=self.arc2.extentC, fill=self.arc2.color)
-    #     canvas_arc3 = mycanvas.create_arc(self.arc3.xy, self.arc3.xy[0] + self.arc3.size[0],
-    #                                       self.arc3.xy[1] + self.arc3.size[1],
-    #                                       start=self.arc3.startC,
-    #                                       extent=self.arc3.extentC, fill=self.arc3.color)
-    #     canvas_arc4 = mycanvas.create_arc(self.arc4.xy, self.arc4.xy[0] + self.arc4.size[0],
-    #                                       self.arc4.xy[1] + self.arc4.size[1],
-    #                                       start=self.arc4.startC,
-    #                                       extent=self.arc4.extentC, fill=self.arc4.color)
-
-    def arc_check_place(self, who):
-        return who.xy
-
-    def arc_check_rotate(self, who):
-        return who.startC
-
     def action(self):
-        self.start = True
-        # added_thread_check_edge = threading.Thread(target=arc_check_edge)
-        # added_thread_check_edge.start()
-        #
-        # # #添加自动移动到线程
-        # added_thread1 = threading.Thread(target=arc1_randmove)
-        # # threads.append(added_thread1)
-        # added_thread1.start()
-        # added_thread1a = threading.Thread(target=arc1_rand_rotate)
-        # # threads.append(added_thread1a)
-        # added_thread1a.start()
-        #
-        # added_thread2 = threading.Thread(target=arc2_randmove)
-        # added_thread2.start()
-        # added_thread2a = threading.Thread(target=arc2_rand_rotate)
-        # added_thread2a.start()
-        #
-        # added_thread3 = threading.Thread(target=arc3_randmove)
-        # added_thread3.start()
-        # added_thread3a = threading.Thread(target=arc3_rand_rotate)
-        # added_thread3a.start()
-        #
-        # added_thread4 = threading.Thread(target=arc4_randmove)
-        # added_thread4.start()
-
-        # added_thread5 = threading.Thread(target=self._arcs.arc_randmove,
-        #                                  args=[self.arcst, self.canvas_arcst, self._mycanvas, ])
-        # added_thread5.start()
-        #
-        # added_thread5a = threading.Thread(target=self._arcs.arc_rand_rotate,
-        #                                   args=[self.arcst, self.canvas_arcst, self._mycanvas, ])
-        # added_thread5a.start()
         for i in self.arcs_list:
             i.event.set()
         print(threading.active_count())
 
     def action_stop(self):
-
         for i in self.arcs_list:
             i.event.clear()
-        self.start = False
